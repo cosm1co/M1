461b595020d7e96292c7489d2922d0fe1575b7b9
cosm1co <federicofaraz@gmail.com>
2022-04-07 11:08:22 -0300

    commit04

--- 04-EstructuraDeDatos-II/homework/homework.js
+++ 04-EstructuraDeDatos-II/homework/homework.js
@@ -11,12 +11,81 @@
 // search: Busca un valor dentro de la lista. Puede recibir un valor o una función. Si no hubiera resultados, devuelve null.
 
 function LinkedList() {
-
+this.head = null;
 }
 
 function Node(value){
-
+this.value = value;
+this.next = null;
 }
+
+  LinkedList.prototype.add = function(x){
+    let newNode = new Node(x) // creamos un nuevo objeto de la clase Node 
+    let current = this.head // asignamos por referencia a la cabeza de la lista
+    if(!current){ // preguntamos si existe this.head
+      this.head = newNode;// asignamos a this.head = newNode
+      return newNode 
+    }
+    while (current.next){ //iteramos sobre la lista. Cuando current.nex es null, corta el while
+      current = current.next;
+    }
+     return current.next = newNode; //asignamos current. next a node
+  }
+
+  // LinkedList{this.head = {this.value = 10; this.next = {this.value = 20; this.next = null}}; this._length = 2}
+
+  LinkedList.prototype.remove = function (){
+    let current = this.head // asignamos a current this.head
+    if(!current){ // lista vacía
+      return null;
+    }
+    if(!current.next){ //
+      let guardado = this.head.value; //guardamos el valor del elemento eliminado
+      this.head = null; //borramos el nodo asignandole a this.head ---> null
+      return guardado; //devolvemos el elemento eliminado
+    }
+    while(current.next.next){
+      current = current.next;
+    }
+    let guardado = current.next.value;
+    current.next = null;
+    return guardado;
+  }
+//                                        c       cn      cnn
+//                         1      2       3       4       null 
+//                                               
+  
+LinkedList.prototype.search = function(arg){
+  let current = this.head;
+  while(current){
+    if(current.value === arg) return current.value
+    if(typeof arg === 'function'){
+      if(arg(current.value)) return current.value
+    }
+    current = current.next;
+  }
+  return null;
+}
+//  if(this.head === null){ //si this.head es igual a null
+//    return null; //devuelve null porque esta vacía la lista
+//  }
+//  let current = this.head;//1 o mas nodos- creamos variable auxiliar current
+//  while(current){ // 
+//    if(current.value === parametro){ // si parametro es un valor normal, retornamos el valor
+//      return current.value
+//    }else if(typeof(parametro) === 'function'){ //Si parametro es una function
+//      if(parametro(current.value)){ //busco el argumento que tiene esta funcion
+//        return current.value;
+//      }
+//    }
+//    current = current.next;
+// }
+// return null;//devuelve null porque no encontró el parametro
+// }
+  
+//                                                c
+//                         1      2       3       4                        
+// current.next.value !== value
 
 // Hash Table( ver información en: https://es.wikipedia.org/wiki/Tabla_hash)
 // Una Hash table contiene un arreglo de "contenedores" o buckets donde puede guardar información.
@@ -31,8 +100,96 @@ function Node(value){
 //    - Retornar dicho valor.
 
 function HashTable() {
-
+  this.numBuckets = 35;
+  this.bucket = [];
 }
+
+HashTable.prototype.hash = function(key){
+  let sum = 0;
+  for(let i = 0; i < key.length; i++){
+    sum = sum + key.charCodeAt(i)
+  }
+  return sum % this.numBuckets
+}
+// key =  3   80   5   7   = 95
+//                     i
+HashTable.prototype.set = function(key, value){
+  if(typeof key !== 'string') throw new TypeError;
+  let posicion = this.hash(key)
+  if(!this.bucket[posicion]){
+    this.bucket[posicion] = {}
+  }
+  this.bucket[posicion][key] = value; 
+}
+
+HashTable.prototype.get = function(key){
+  let posicion = this.hash(key);
+  return this.bucket[posicion][key];
+}
+
+HashTable.prototype.hasKey = function(key){
+  let posicion = this.hash(key);
+  return this.bucket[posicion].hasOwnProperty(key)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// HashTable.prototype.hash = function (key){
+// // esta funcion hasheadora deberia sumar los key code de las letras de la palabra,
+//     // y hacer el mod de ese numero por el numero de buckets .
+//     let sum = 0
+//     for(let i = 0; i < key.length; i++){
+//       sum += key.chartCodeAt(i)
+//     }
+//     return sum % this.numBuckets;
+// }
+
+// HashTable.prototype.set = function(key, value) {
+//   if(typeof key !== 'string'){
+//     throw new TypeError, ('Keys must be strings')
+//   }
+//   let i = this.hash(key)
+//   if(!this.bucket[i]){
+//     this.bucket[i] = {}
+//   }
+//   this.bucket[i][key] = value;
+// }
+
+// HashTable.prototype.get = function(key) {
+//   let i = this.hash(key);
+//   return this.bucket[i] ? this.bucket[i][key] : undefined 
+// }
+
+// HashTable.prototype.hasKey = function(key) {
+//   var i = this.hash(key)
+//   return this.bucket[i].hasOwnProperty(key) 
+// }
 
 
 // No modifiquen nada debajo de esta linea
--- 05-EstructuraDeDatos-III/homework/homework.js
+++ 05-EstructuraDeDatos-III/homework/homework.js
@@ -8,10 +8,74 @@
 // corre depth-first (en recorrido "post-order" cuando depthFirstForEach() es ejecutado con la opcion "post-order"
 // corre breadth-first cuando breadthFirstForEach() es ejecutado
 // Observar imagen de la carpeta "homework" llamada "bst.png". Allí encontraran dibujado el arbol utilizado para los tests
-function BinarySearchTree() {
-
+function BinarySearchTree(value) {
+this.right = null,
+this.left = null;
+this.value = value;
 }
 
+BinarySearchTree.prototype.insert = function(value){
+if(value > this.value){ //pregunto si value es mayor que mi raíz(this.value)- a la derecha si es mayor
+    if(this.right) this.right.insert(value);//si la rama NO ESTÁ VACÍA --> RECURSIVIDAD
+    else this.right = new BinarySearchTree(value)// si está VACÍA --> agrego Nuevo Árbol.
+} else { //si no se cumple la primera condición entra en este else IZQUIERDA
+    if(this.left) this.left.insert(value);//si la rama izq NO ESTÁ VACÍA --> recursividad
+    else this.left = new BinarySearchTree(value);// si está VACÍA --> agrego Nuevo Árbol.
+}
+}
+BinarySearchTree.prototype.contains = function(value){
+  if(this.value === value) return true;// si encuentra el valor devuelve true
+  if(value > this.value){ //si value es mayor a this.value se va a la derecha
+    return !!this.right && this.right.contains(value); //si this.right es false no hace recursividad y termina de buscar
+  }
+  return !!this.left && this.left.contains(value)// si value es mejor que this.value pasa a esta linea
+  // if(this.value === arg) return true;//si arg es igual this.value
+  // if(arg > this.value){// si arg es mayor this.value
+  //   if(!this.right) return false;// si no hay nada en la derecha retorna falso
+  //   else return this.right.contains(arg)//
+  // }else{ 
+    //   if(!this.left) return false;
+    //   else return this.left.contains(arg)// retorna la posición en la derecha con recursividad y el mismo dato 
+    // }
+  }
+BinarySearchTree.prototype.depthFirstForEach = function(cb, order){
+// Pre-order --> root - izq - der
+// In-order --> izq - root - der
+// Post-order --> izq - der - root
+if(order === 'pre-order'){
+  cb(this.value);
+  if(this.left !== null) this.left.depthFirstForEach(cb, order);
+  if(this.right !== null) this.right.depthFirstForEach(cb, order);
+ } else if(order === 'post-order'){
+    if(this.left !== null) this.left.depthFirstForEach(cb, order);
+    if(this.right !== null) this.right.depthFirstForEach(cb, order);
+    cb(this.value);
+  } else {
+    if(this.left !== null) this.left.depthFirstForEach(cb, order);
+    cb(this.value)
+    if(this.right !== null) this.right.depthFirstForEach(cb, orrder);
+  }
+
+}
+BinarySearchTree.prototype.breadthFirstForEach = function(cb, array=[]){
+  if(this.left){
+    array.push(this.left)
+  }
+  if(this.right){
+    array.push(this.right);
+  }
+  cb(this.value);
+
+  if(array.length > 0){
+    array.shift().breadthFirstForEach(cb, array);
+  }
+};
+BinarySearchTree.prototype.size = function(){
+if (!this.right && !this.left) return 1;
+if (!this.right) return 1 + this.left.size();
+if (!this.left) return 1 + this.right.size();
+return 1 + this.left.size() + this.right.size();
+}
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
